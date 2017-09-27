68eed953a807ffe8e8783a80650aa7fbc3c8174f
Maria <malett@ttu.ee>
2017-09-27 17:39:23 +0300

    1. vahetähtaeg

---
+++ src/test/java/weatherproject/WeatherProjectTestSystem.java
@@ -0,0 +1,25 @@
+package weatherproject;
+import org.junit.Test;
+
+/**
+ * Tests for the weather project.
+ */
+public class WeatherProjectTestSystem {
+    @Test
+    public void testIfCoordinatesAreValid() { }
+
+    @Test
+    public void testIfTemperatureIsValid() { }
+
+    @Test
+    public void testForecastHighestTemperatureInADay() { }
+
+    @Test
+    public void testForecastLowestTemperatureInADay() { }
+
+    @Test
+    public void testIfRequestedCityIsInResponse() { }
+
+    @Test
+    public void testConnectionToAPI() { }
+}
